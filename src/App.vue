23703907d195a1c5df7b21ec99d63306f038db94
andrearicciotti <ricciotti.andrea1993@gmail.com>
2024-02-02 10:18:34 +0100

    add: axios call to api

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <script>
+import AppMain from "./components/AppMain.vue";
+
 export default {
   data() {
-    return
+    return {};
+  },
+  components: {
+    AppMain,
   }
 }
 </script>
 
 <template>
-
+  <div class="row text-center">
+    <h1>wewe</h1>
+    <AppMain />
+  </div>
 </template>
 
-<style lang="scss" scoped>
-@use './styles/general.scss';
+<style lang="scss">
+@use './assets/styles/general.scss' as *;
 
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            baseUrl: 'http://127.0.0.1:8000',
+            projects: [],
+        }
+    },
+    created() {
+        axios.get(`${this.baseUrl}/api/projects`)
+            .then((resp) => {
+            this.projects = resp.data;
+            console.log(this.projects);
+        });
+    },
+}
+</script>
+<template>
+    <div>
+        <h2>heeee</h2>
+    </div>
+</template>
+<style lang="scss" scoped>
+    
+</style>
